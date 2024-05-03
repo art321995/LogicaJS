d6913870ebdc39a41c62e983c0f31425ca10ea43
Arturo Esqueda <art270112@gmail.com>
2024-05-03 14:47:52 -0600

    Ejercicios de Logica - Terminados

---
+++ test-ejercicios/ejercicio2/ejercicio2.js
@@ -0,0 +1,6 @@
+function palindromo (texto){
+    let invertido = texto .split('') .reverse() .join('');
+    return (invertido === texto);
+};
+
+module.exports = palindromo;
---
+++ test-ejercicios/ejercicio2/ejercicio2.test.js
@@ -0,0 +1,6 @@
+
+const palindromo = require("./ejercicio2");
+
+test("Comprobar si la funcion palindromo esta definida", () =>{
+    expect(palindromo).toBeDefined();
+});
